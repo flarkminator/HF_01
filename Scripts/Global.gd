ea6288f50e94c7ab91d5663fa2ed6b211525dd76
flarkminator <flarkminator@gmail.com>
2019-05-14 10:09:25 -0700

    Setting up the physics for the tennis ball. Very first try.

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,7 +1,23 @@
 extends Node
+#warning-ignore-all:unused_class_variable
 
-var PixelsPerMeter = 32
+var PixelsPerMeter_Y = 16
+var PixelsPerMeter_X = 32
 
-func MeterToPixel(Meters : int) -> int:
-	return int(Meters/PixelsPerMeter)
+var Gravity = -9.8 # m/s^2
+var AirDensity = 1.21 # kg/m^3
+
+
+func MeterToPixel(X : int, Y : int) -> Array:
+	return [int(X/PixelsPerMeter_X), int(Y/PixelsPerMeter_Y)]
+
+# Formula for the Magnus Force is Fm = CoefficientForLift * SurfaceArea * DensityOfMedium * Velocity^2
+# The force is perpindicular to the forward vector; vertical for backspin, downwards for topspin
+func CalculateMagnusForce():
+	pass
+
+# Formula for the Drag Force is Fd = CoefficientForDrag * SurfaceArea * DensityOfMedium * Velocity^2
+# The force is always in direct opposition to the forward vector
+func CalculateDragForce():
+	pass
 
---
+++ Scripts/TennisBall.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var DragCoefficient = 0.55 # Scalar
+var Mass = 0.055 # kg
+var Radius = 0.033 # Meters
+var Circumfrence = 0.003421194 # Meters^2
+
+
+func _ready():
+	pass
